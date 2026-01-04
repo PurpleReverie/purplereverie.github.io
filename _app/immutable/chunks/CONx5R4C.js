e4dc108476676476a7132aaad9ccfb2aed507ffe
TauraJ <taurajgreig@outlook.com>
2026-01-04 22:16:31 +1300

    latest build

--- _app/immutable/chunks/CONx5R4C.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1i5k2lz)==null?void 0:s.base)??"/offical_resume";var e;const l=((e=globalThis.__sveltekit_1i5k2lz)==null?void 0:e.assets)??a;export{l as a,a as b};
---
+++ _app/immutable/entry/start.DWL-50qn.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/sZ7R_pr3.js";export{t as start};
